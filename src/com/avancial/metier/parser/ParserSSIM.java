d57e28af9773e36ac3a60e5cdd8b8587fd1f9115
Bruno LE GLOAHEC <bruno.legloahec@avancial.com>
2015-06-05 18:14:22 +0200

    Nettoyage

--- src/com/avancial/metier/parser/ParserSSIM.java
+++ src/com/avancial/metier/parser/ParserSSIM.java
@@ -5,43 +5,41 @@ import com.avancial.parser.IParser;
 
 /**
  * 
- * @author ismael.yahiani 
- * cette class permet de parser un fichier SSIM en vue d'alimenté l'objet métier   
- *elle etend la classe abstraite AParser   
+ * @author ismael.yahiani cette class permet de parser un fichier SSIM en vue d'alimenté l'objet métier elle etend la classe abstraite AParser
  */
 public class ParserSSIM extends AParser {
 
-	public ParserSSIM(IParser par) {
-		super(par);
-	}
-	
-	@Override
-	public String parse(String ligne) {
+   public ParserSSIM(IParser par) {
+      super(par);
+   }
 
-		StringBuilder sb = new StringBuilder() ;
-		
-		if (!ligne.isEmpty()) {
-			if (this.parser != null)
-				this.resultat = parser.parse(ligne);
-			if(!resultat.isEmpty()) {
+   @Override
+   public String parse(String ligne) {
 
-				sb.append(resultat.subSequence(TGVAIR_enumParserSSIM.POSITION_TYPE_ENR.getPositionDebut(), TGVAIR_enumParserSSIM.POSITION_TYPE_ENR.getPositionFin()));
-				sb.append(resultat.subSequence(TGVAIR_enumParserSSIM.POSITION_IDENTIFIANT_TRANCHE.getPositionDebut(), TGVAIR_enumParserSSIM.POSITION_IDENTIFIANT_TRANCHE.getPositionFin()));
-				sb.append(resultat.subSequence(TGVAIR_enumParserSSIM.POSITION_RANG_TRANCON.getPositionDebut(), TGVAIR_enumParserSSIM.POSITION_RANG_TRANCON.getPositionFin()));
-				sb.append(resultat.subSequence(TGVAIR_enumParserSSIM.POSITION_PERIODE_CIRCULATION.getPositionDebut(), TGVAIR_enumParserSSIM.POSITION_PERIODE_CIRCULATION.getPositionFin()));
-				sb.append(resultat.subSequence(TGVAIR_enumParserSSIM.POSITION_JOURS_CIRCULATION.getPositionDebut(), TGVAIR_enumParserSSIM.POSITION_JOURS_CIRCULATION.getPositionFin()));
-				sb.append(resultat.subSequence(TGVAIR_enumParserSSIM.POSITION_GARE_DEPART.getPositionDebut(), TGVAIR_enumParserSSIM.POSITION_GARE_DEPART.getPositionFin()));
-				sb.append(resultat.subSequence(TGVAIR_enumParserSSIM.POSITION_HEURE_DEPART.getPositionDebut(), TGVAIR_enumParserSSIM.POSITION_HEURE_DEPART.getPositionFin()));
-				sb.append(resultat.subSequence(TGVAIR_enumParserSSIM.POSITION_DIFFERENCE_GMT_DEPART.getPositionDebut(), TGVAIR_enumParserSSIM.POSITION_DIFFERENCE_GMT_DEPART.getPositionFin()));
-				sb.append(resultat.subSequence(TGVAIR_enumParserSSIM.POSITION_GARE_ARRIVER.getPositionDebut(), TGVAIR_enumParserSSIM.POSITION_GARE_ARRIVER.getPositionFin()));
-				sb.append(resultat.subSequence(TGVAIR_enumParserSSIM.POSITION_HEURE_ARRIVER.getPositionDebut(), TGVAIR_enumParserSSIM.POSITION_HEURE_ARRIVER.getPositionFin()));
-				sb.append(resultat.subSequence(TGVAIR_enumParserSSIM.POSITION_DIFFERENCE_GMT_ARRIVER.getPositionDebut(), TGVAIR_enumParserSSIM.POSITION_DIFFERENCE_GMT_ARRIVER.getPositionFin()));
-				sb.append(resultat.subSequence(TGVAIR_enumParserSSIM.POSITION_INDICATEUR_FER.getPositionDebut(), TGVAIR_enumParserSSIM.POSITION_INDICATEUR_FER.getPositionFin()));
-				sb.append(resultat.subSequence(TGVAIR_enumParserSSIM.POSITION_COMPAGNIE_TRAIN.getPositionDebut(), TGVAIR_enumParserSSIM.POSITION_COMPAGNIE_TRAIN.getPositionFin()));
-				sb.append(resultat.subSequence(TGVAIR_enumParserSSIM.POSITION_NUM_TRAIN.getPositionDebut(), TGVAIR_enumParserSSIM.POSITION_NUM_TRAIN.getPositionFin()));
-			
-			}	
-		}
-		return sb.toString() ;
-	}
+      StringBuilder sb = new StringBuilder();
+
+      if (!ligne.isEmpty()) {
+         if (this.parser != null)
+            this.resultat = this.parser.parse(ligne);
+         if (!this.resultat.isEmpty()) {
+
+            sb.append(this.resultat.subSequence(TGVAIR_enumParserSSIM.POSITION_TYPE_ENR.getPositionDebut(), TGVAIR_enumParserSSIM.POSITION_TYPE_ENR.getPositionFin()));
+            sb.append(this.resultat.subSequence(TGVAIR_enumParserSSIM.POSITION_IDENTIFIANT_TRANCHE.getPositionDebut(), TGVAIR_enumParserSSIM.POSITION_IDENTIFIANT_TRANCHE.getPositionFin()));
+            sb.append(this.resultat.subSequence(TGVAIR_enumParserSSIM.POSITION_RANG_TRANCON.getPositionDebut(), TGVAIR_enumParserSSIM.POSITION_RANG_TRANCON.getPositionFin()));
+            sb.append(this.resultat.subSequence(TGVAIR_enumParserSSIM.POSITION_PERIODE_CIRCULATION.getPositionDebut(), TGVAIR_enumParserSSIM.POSITION_PERIODE_CIRCULATION.getPositionFin()));
+            sb.append(this.resultat.subSequence(TGVAIR_enumParserSSIM.POSITION_JOURS_CIRCULATION.getPositionDebut(), TGVAIR_enumParserSSIM.POSITION_JOURS_CIRCULATION.getPositionFin()));
+            sb.append(this.resultat.subSequence(TGVAIR_enumParserSSIM.POSITION_GARE_DEPART.getPositionDebut(), TGVAIR_enumParserSSIM.POSITION_GARE_DEPART.getPositionFin()));
+            sb.append(this.resultat.subSequence(TGVAIR_enumParserSSIM.POSITION_HEURE_DEPART.getPositionDebut(), TGVAIR_enumParserSSIM.POSITION_HEURE_DEPART.getPositionFin()));
+            sb.append(this.resultat.subSequence(TGVAIR_enumParserSSIM.POSITION_DIFFERENCE_GMT_DEPART.getPositionDebut(), TGVAIR_enumParserSSIM.POSITION_DIFFERENCE_GMT_DEPART.getPositionFin()));
+            sb.append(this.resultat.subSequence(TGVAIR_enumParserSSIM.POSITION_GARE_ARRIVER.getPositionDebut(), TGVAIR_enumParserSSIM.POSITION_GARE_ARRIVER.getPositionFin()));
+            sb.append(this.resultat.subSequence(TGVAIR_enumParserSSIM.POSITION_HEURE_ARRIVER.getPositionDebut(), TGVAIR_enumParserSSIM.POSITION_HEURE_ARRIVER.getPositionFin()));
+            sb.append(this.resultat.subSequence(TGVAIR_enumParserSSIM.POSITION_DIFFERENCE_GMT_ARRIVER.getPositionDebut(), TGVAIR_enumParserSSIM.POSITION_DIFFERENCE_GMT_ARRIVER.getPositionFin()));
+            sb.append(this.resultat.subSequence(TGVAIR_enumParserSSIM.POSITION_INDICATEUR_FER.getPositionDebut(), TGVAIR_enumParserSSIM.POSITION_INDICATEUR_FER.getPositionFin()));
+            sb.append(this.resultat.subSequence(TGVAIR_enumParserSSIM.POSITION_COMPAGNIE_TRAIN.getPositionDebut(), TGVAIR_enumParserSSIM.POSITION_COMPAGNIE_TRAIN.getPositionFin()));
+            sb.append(this.resultat.subSequence(TGVAIR_enumParserSSIM.POSITION_NUM_TRAIN.getPositionDebut(), TGVAIR_enumParserSSIM.POSITION_NUM_TRAIN.getPositionFin()));
+
+         }
+      }
+      return sb.toString();
+   }
 }
