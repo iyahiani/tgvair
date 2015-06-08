311db302c9339103774960d6c1a06966f664ba2f
Bruno LE GLOAHEC <bruno.legloahec@avancial.com>
2015-06-08 17:00:03 +0200

    On inverse les filtrages

--- src/com/avancial/metier/parser/FilterSSIMTypeEnr.java
+++ src/com/avancial/metier/parser/FilterSSIMTypeEnr.java
@@ -16,14 +16,14 @@ public class FilterSSIMTypeEnr extends AFilter {
 
    @Override
    public String parse(String ligne) {
-      String chaine = "";
-      if (chaine.substring(TGVAIR_enumParserSSIM.POSITION_TYPE_ENR.getPositionDebut(), TGVAIR_enumParserSSIM.POSITION_TYPE_ENR.getPositionFin()).equals(TGVAIR_enumParserSSIM.VALEUR_TYPE4.getTypeEnr()))
-         chaine = "";
-      // Si parser est null ou que la chaine est vide, on interrompt le process
-      if (this.parser != null && !chaine.isEmpty())
-         chaine = this.parser.parse(ligne);
 
-      return chaine;
+      if (ligne.substring(TGVAIR_enumParserSSIM.POSITION_TYPE_ENR.getPositionDebut(), TGVAIR_enumParserSSIM.POSITION_TYPE_ENR.getPositionFin()).equals(TGVAIR_enumParserSSIM.VALEUR_TYPE4.getTypeEnr()))
+         ligne = "";
+      // Si parser est null ou que la chaine est vide, on interrompt le process
+      if (this.parser != null && !ligne.isEmpty())
+         ligne = this.parser.parse(ligne);
+
+      return ligne;
    }
 }
 // /TGVAIR_enumParserSSIM.VALEUR_TYPE4
