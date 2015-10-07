83a2d670a3e25ea5e9d07df4f93361c4898f1f7f
Bruno LE GLOAHEC <bruno.legloahec@avancial.com>
2015-10-07 12:32:14 +0200

    Versionning application

--- src/com/avancial/app/model/managedbean/ParamGetterManagedBean.java
+++ src/com/avancial/app/model/managedbean/ParamGetterManagedBean.java
@@ -6,15 +6,15 @@ package com.avancial.app.model.managedbean;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Named;
 
+import com.avancial.app.resources.constants.APP_TgvAir;
 import com.avancial.socle.params.AParamGetter;
 import com.avancial.socle.params.IParamReader;
+import com.avancial.socle.params.ParamReaderFileGeneric;
 import com.avancial.socle.params.beans.IParamBean;
 
 /**
- * Classe implémentant un point unique de gestion des paramètres. Inspiré du
- * design patttern Facade. <br>
- * Les paramètres du socle sont chargés dans la classe abstraite. Pour ajouter
- * ceux de l'application, il suffit de compléter le constructeur.<br/>
+ * Classe implémentant un point unique de gestion des paramètres. Inspiré du design patttern Facade. <br>
+ * Les paramètres du socle sont chargés dans la classe abstraite. Pour ajouter ceux de l'application, il suffit de compléter le constructeur.<br/>
  * Voir {@link IParamBean} , {@link IParamReader}
  * 
  * @author bruno
@@ -31,6 +31,10 @@ public class ParamGetterManagedBean extends AParamGetter {
     */
    public ParamGetterManagedBean() throws Exception {
       super();
+      // On instancie les Paramètres de l'appli
+      ParamReaderFileGeneric app = new ParamReaderFileGeneric(this.getPathToWebInf() + APP_TgvAir.CHEMIN_APP_PROPERTIES.toString());
+      app.loadParams("app");
+      this.add(app);
    }
 
 }
